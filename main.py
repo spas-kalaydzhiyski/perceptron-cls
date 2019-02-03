972aa7d104cd4ef006eafeafb9043d7fbb0e342c
root <root@localhost.localdomain>
2019-02-03 17:05:03 +0000

    refactored perceptron and removed unnecessary code from main.py

--- main.py
+++ main.py
@@ -7,103 +7,77 @@ from perceptron import Perceptron
 
 
 def plot_decision_regions(X, y, classifier, resolution=0.02):
-    # markers for each flower type
-    markers = ['s', 'x', 'o', '^', 'v']
-    colours = ['red', 'green', 'blue', 'cyan', 'magenta']
-    n_flower_types = len(np.unique(y))
-    colour_map = ListedColormap(colours[:n_flower_types])
-    
-    # plot decision surface
-    X1_min, X1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    X2_min, X2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(X1_min, X1_max, resolution), np.arange(X2_min, X2_max, resolution)) 
+  # markers for each flower type
+  markers = ['s', 'x', 'o', '^', 'v']
+  colours = ['red', 'green', 'blue', 'cyan', 'magenta']
+  n_flower_types = len(np.unique(y))
+  colour_map = ListedColormap(colours[:n_flower_types])
+  
+  # plot decision surface
+  X1_min, X1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+  X2_min, X2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+  xx1, xx2 = np.meshgrid(np.arange(X1_min, X1_max, resolution), np.arange(X2_min, X2_max, resolution)) 
 
-    pred = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    pred = pred.reshape(xx1.shape) 
+  pred = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+  pred = pred.reshape(xx1.shape) 
 
-    plt.figure(3)
-    plt.title('Decision boundary')
-    plt.contourf(xx1, xx2, pred, alpha=0.3, cmap=colour_map)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-    # plot all class samples
-    for idx, cls in enumerate(np.unique(y)):
-        plt.scatter(x=X[y==cls, 0],
-                    y=X[y==cls, 1],
-                    alpha=0.8,
-                    c=colours[idx],
-                    label=cls,
-                    edgecolor='black')
+  plt.figure(3)
+  plt.title('Decision boundary')
+  plt.contourf(xx1, xx2, pred, alpha=0.3, cmap=colour_map)
+  plt.xlim(xx1.min(), xx1.max())
+  plt.ylim(xx2.min(), xx2.max())
+  # plot all class samples
+  for idx, cls in enumerate(np.unique(y)):
+      plt.scatter(x=X[y==cls, 0],
+                  y=X[y==cls, 1],
+                  alpha=0.8,
+                  c=colours[idx],
+                  label=cls,
+                  edgecolor='black')
 
 
 def plot(X, y, flower_label1=1, flower_label2=2):
-    plt.figure(1)
-    plt.title('Starting scattered data')
-    plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label=flower_label1)
-    plt.scatter(X[50:, 0], X[50:, 1], color='blue', marker='x', label=flower_label2)
-    plt.xlabel('sepal length (cm)')
-    plt.ylabel('petal length (cm)')
-    plt.legend(loc='upper left')
+  print('Separation of 2D features per flower type...')
+  plt.figure(1)
+  plt.title('Starting scattered data')
+  plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label=flower_label1)
+  plt.scatter(X[50:, 0], X[50:, 1], color='blue', marker='x', label=flower_label2)
+  plt.xlabel('sepal length (cm)')
+  plt.ylabel('petal length (cm)')
+  plt.legend(loc='upper left')
 
 
 def train(X, y):
-    model = Perceptron(learning_rate=0.1, n_iterations=10)
-    model.fit(X, y)
-    plt.figure(2)
-    plt.title('Prediction error decreasing over time')
-    plt.plot(range(1, len(model.errors) + 1), model.errors, marker='o')
-    plt.xlabel('Epochs')
-    plt.ylabel('Number of weight updates') # we only update when there is an error in prediction
-    print('Number of errors per epoch (should be decreasing)')
-    plot_decision_regions(X, y, classifier=model)
-    plt.show()
+  classifier = Perceptron(learning_rate=0.1, n_epochs=10)
+  classifier.fit(X, y)
+  plt.figure(2)
+  plt.title('Prediction error decreasing over time')
+  time_steps = range(1, len(classifier.get_errors()) + 1)
+  plt.plot(time_steps, classifier.get_errors(), marker='o')
+  plt.xlabel('Epochs')
+  plt.ylabel('Number of weight updates') # we only update when there is an error in prediction
+  print('Number of errors per epoch (should be decreasing)')
+  plot_decision_regions(X, y, classifier=classifier)
+  plt.show()
 
 
-def start(df, flower_type1=1, flower_type2=2):
-    """
-    Select the types flowers you want to classify
-    * initially classifying setosa and versicolor
-    """
-    if flower_type1 == 2 and flower_type2 == 3:
-        y = df.iloc[50:150, 4].values
-        y = np.where(y == 'Iris-versicolor', -1, 1)
-        X = df.iloc[50:150, [0, 2]].values  # extract only sepal_length and petal length
-        flower_label1 = 'versicolor'
-        flower_label2 = 'virginica'
-    elif flower_type1 == 1 and flower_type2 == 3:
-        # join labels from start and end of dataframe
-        y_ft1 = df.iloc[:50, 4] 
-        y_ft2 = df.iloc[100:150, 4]
-        y = pd.concat([y_ft1, y_ft2]).values
-        y = np.where(y == 'Iris-setosa', -1, 1)
-        # join features from start and end of dataframe
-        X_ft1 = df.iloc[:50, [0, 2]]
-        X_ft2 = df.iloc[100:150, [0, 2]]
-        X = pd.concat([X_ft1, X_ft2]).values
-        flower_label2 = 'virginica'
-    else:
-        y = df.iloc[:100, 4].values
-        y = np.where(y == 'Iris-setosa', -1, 1)
-        X = df.iloc[:100, [0, 2]].values
-        flower_label1 = 'setosa'
-        flower_label2 = 'versicolor'
+def start(df):
+  """
+  Select the types flowers you want to classify
+  * initially classifying setosa and versicolor
+  """
+  y = df.iloc[:100, 4].values
+  y = np.where(y == 'Iris-setosa', -1, 1)
+  X = df.iloc[:100, [0, 2]].values
 
-    
-    #print('features: \n{} ...'.format(X[:10]))
-    #print('labels: \n{} ...'.format(y[:10]))
-    print('Separation of 2D features per flower type...')
-    plot(X, y, flower_label1, flower_label2)
-    train(X, y)
-
+  #plot(X, y, 'setosa', 'versicolor')
+  train(X, y)
 
 def main():
-    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
-    print('DataFrame shape {}'.format(df.shape))
-    print('Select which two types of flowers you would like to classify split by ",":')
-    ft1, ft2 = (int(x) for x in input('1 - setosa\n2 - versicolor\n3 - versinica\n~> ').split(','))
-    start(df, ft1, ft2)
-
+  df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+  print('DataFrame shape {}'.format(df.shape))
+  start(df)
 
 if __name__ == '__main__':
-    main()
+  main()
 
--- perceptron.py
+++ perceptron.py
@@ -2,45 +2,53 @@ import numpy as np
 
 
 class Perceptron:
+  """
+  Class to represent one simple learning mechanism similar to
+  real neurons in the human brain, the perceptron takes inputs
+  and decides based on them whether to fire or not.
+  """
+  def __init__(self, learning_rate=0.01, n_epochs=64, random_state=1):
+    self.learning_rate = learning_rate
+    self.n_epochs = n_epochs
+    self.random_state = random_state
+    self.errors = []    #used to store the errors per training epoch
+
+  def fit(self, X, y):
     """
-    Perceptron clasifier:
-    @random_state is needed to generate the random state of the weights vector
+    Function to fit the model on the all the training samples.
+    X = fetures matrix -               matrix (n_samples, n_features)
+    y = target label for each sample - vector (n_samples)
     """
-    def __init__(self, learning_rate=0.01, n_iterations=50, random_state=1):
-        self.learning_rate = learning_rate
-        self.n_iterations = n_iterations
-        self.random_state = random_state
-        self.errors = []
+    rand_gen = np.random.RandomState(self.random_state)
+    weights_vec_size = X.shape[1] + 1
+    self.weights = rand_gen.normal(loc=0.0, scale=0.01, size=weights_vec_size)
+    for _ in range(self.n_epochs):
+      error = 0
+      for xi, target in zip(X, y):
+        update = self.learning_rate * (self.predict(xi) - target)
+        self.weights[1:] += update * xi 
+        self.weights[0] += update
+        error += int(update != 0)
+      self.errors.append(error)
 
-    def fit(self, X, y):
-        """
-        Fit the training data for classification.
-        @X - features matrix (n_samples X n_features)
-        @y - correct labels for each sample (n_samples)
-        """
-        rand_gen = np.random.RandomState(self.random_state)
-        # the weights have 1 more element than n_features to accomodate
-        # for the bias unit w0*x0 where x0 = 1 and the threshold is 0
-        w_vec_size = 1 + X.shape[1]
-        self.w = rand_gen.normal(loc=0.0, scale=0.01, size=w_vec_size)
-        for _ in range(self.n_iterations):
-            errors = 0
-            for xi, target in zip (X, y):
-                # each sample xi is a vector of shape (n_features, )
-                update = self.learning_rate * (target - self.predict(xi))
-                self.w[0] += update     # since xi in this case is x0=1
-                self.w[1:] += update * xi
-                errors += int(update != 0.0)
-            self.errors.append(errors)
+  def get_errors(self):
+    return self.errors
 
-        return self
+  def get_all_weight_update(self):
+    return self.weight_updates
+  
+  def net_input(self, xi):
+    """
+    return the net_input of the weights and the current
+    values of the sample being processed during training.
+    """
+    return np.dot(xi, self.weights[1:]) + self.weights[0]
 
-    def net_input(self, sample):
-        return np.dot(sample, self.w[1:]) + self.w[0]
+  def predict(self, xi):
+    """
+    utilises a threshold (0 in this case) to classify the label
+    for the inputs and the current state of the weights and biases.
+    """
+    return np.where(self.net_input(xi) >= 0, 1, -1)  
 
-    def predict(self, sample):
-        """
-        Returns the predicted class label after the unit-step function
-        """
-        return np.where(self.net_input(sample) >= 0, 1, -1)
 
